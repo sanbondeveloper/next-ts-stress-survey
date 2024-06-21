deb907a351567924029dc9e2726edb6a8a28a4e2
sanbondeveloper <sanbondeveloper@gmail.com>
2024-06-21 16:43:18 +0900

    Feat: 에러 핸들링 구현

--- util/http.ts
+++ util/http.ts
@@ -3,7 +3,7 @@ import { Question } from '@/types/question';
 export async function fetchQuestions(): Promise<Question[]> {
   const response = await fetch('http://localhost:3000/data/questions.json');
 
-  if (response.ok) new Error('Failed to Fetch Questions');
+  if (response.ok) throw new Error('Failed to Fetch Questions');
 
   const { data }: { data: Question[] } = await response.json();
 
