577fda0fe9b2464227c07c8463f6065f4d46b37e
sanbondeveloper <sanbondeveloper@gmail.com>
2024-06-21 16:48:55 +0900

    Refactor: 파일 위치 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import RecoilRootWrapper from '@/components/recoil-wrapper';
+import RecoilRootWrapper from '@/components/common/recoil-wrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useSetRecoilState } from 'recoil';
 import { userState } from '@/store/userState';
 import { isNotEmpty } from '@/util/validation';
 import { useRouter } from 'next/navigation';
-import Input from '@/components/input';
+import Input from '@/components/common/input';
 
 export default function Home() {
   const [enteredValues, setEnteredValues] = useState({
@@ -32,15 +32,15 @@ export default function Home() {
 
   return (
     <main>
-      <header className="flex flex-col items-center px-4">
-        <h1 className="text-4xl font-bold text-white">스트레스 자가진단</h1>
-        <p>
-          최근 1개월 동안 당신이 느끼고 생각한 것에 대한 것입니다. 각 문항에 해당하는 내용을 얼마나 자주 느꼈는지 표기해
-          주십시오.
-        </p>
-      </header>
+      <form className="max-w-[1024px]" onSubmit={handleSubmit}>
+        <header className="mb-10 flex w-full flex-col items-center px-4">
+          <h1 className="text-4xl font-bold text-white">스트레스 자가진단</h1>
+          <p>
+            최근 1개월 동안 당신이 느끼고 생각한 것에 대한 것입니다. 각 문항에 해당하는 내용을 얼마나 자주 느꼈는지
+            표기해 주십시오.
+          </p>
+        </header>
 
-      <form className="mt-10" onSubmit={handleSubmit}>
         <Input
           label=""
           id="team"
